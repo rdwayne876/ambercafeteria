f307ea8acddb270f16719d7865bce8c5ef06493e
Donte Patterson <pp.donte@yahoo.com>
2022-05-17 14:48:27 -0500

    Co-authored-by: rdwayne876 <rdwayne876@users.noreply.github.com> Co-authored-by: Dejhon <Dejhon@users.noreply.github.com>

--- app.js
+++ app.js
@@ -1,27 +1,21 @@
+const express = require("express");
+require("dotenv").config();
+const app = express();
+const layout = require("express-ejs-layouts");
+const path = require("path");
 
-const express = require('express')
-require('dotenv').config()
-const app = express()
-const layout = require('express-ejs-layouts')
-const path = require('path');
+app.use(layout);
+app.use(express.static("public"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use("/images", express.static("images"));
 
-app.use(layout)
-app.use(express.static('public'));
-app.use(express.json())
-app.use(express.urlencoded({ extended: true }))
-app.use('/images', express.static('images'));
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+app.set("layout", "layouts/layout");
 
+const indexRoute = require("./routes/index");
+app.use("/", indexRoute);
 
-
-app.set('view engine', 'ejs')
-app.set('views', path.join(__dirname, 'views'));
-app.set('layout', 'layouts/layout')
-
-
-
-const indexRoute = require('./routes/index')
-app.use('/', indexRoute)
-
-
-app.listen(process.env.APP_PORT)
-console.log('Server is listening');
+app.listen(process.env.APP_PORT, "10.44.16.13");
+console.log("Server is listening");
--- db.js
+++ db.js
@@ -1,12 +1,13 @@
-// const mysql = require('mysql')
-// require('dotenv').config()
+const mysql = require("mysql");
+require("dotenv").config();
 
-// const connection = mysql.createConnection({
-//     host: process.env.DB_HOST,
-//     user: process.env.DB_USER,
-//     password: process.env.DB_PASS,
-//     database: process.env.DB_NAME,
-// })
+const connection = mysql.createConnection({
+  host: process.env.DB_HOST,
+  port: process.env.APP_PORT,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
+});
 
-// connection.connect()
-// module.exports = connection
+connection.connect();
+module.exports = connection;
