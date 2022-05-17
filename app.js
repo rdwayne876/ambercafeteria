6ad7101b459a9de29af1ab808da9bed71afc00de
Dejhon <dejhonsherman6@gmail.com>
2022-05-16 21:57:29 -0500

    Dejhon added a route for posting student order.

--- app.js
+++ app.js
@@ -1,14 +1,17 @@
 const express =  require('express')
+const req = require('express/lib/request')
 require('dotenv').config()
 const app = express()
 
-const indexRoute = require('/routes/index')
+const indexRoute = require('./routes/index');
+const orderRoute = require('./routes/order');
 
 app.set('view engine', 'ejs')
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
 
-app.use('/', indexRoute)
+app.use('/', indexRoute);
+app.use('/addorder', orderRoute);
 
 app.listen(process.env.APP_PORT)
 console.log('Server is listening');
