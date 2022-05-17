8541094a2c90a9c0c0da558b1e61c5ca41abd115
Tyrese <jrackfinn@gmail.com>
2022-05-16 23:32:40 -0500

    update

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+
 const express = require('express')
 require('dotenv').config()
 const app = express()
@@ -11,13 +12,16 @@ app.use(express.urlencoded({ extended: true }))
 app.use('/images', express.static('images'));
 
 
+
 app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname, 'views'));
 app.set('layout', 'layouts/layout')
 
 
+
 const indexRoute = require('./routes/index')
 app.use('/', indexRoute)
+
 
 app.listen(process.env.APP_PORT)
 console.log('Server is listening');
